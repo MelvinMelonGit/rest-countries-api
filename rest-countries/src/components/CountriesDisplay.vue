69ae3451e505c33bddd6f0959517ce7c8fc2e993
MelvinMelonGit <83698963+MelvinMelonGit@users.noreply.github.com>
2024-05-20 23:17:14 +0800

    added fonts

---
+++ rest-countries/src/components/CountriesDisplay.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <main>this is main</main>
+</template>
+
+<style scoped>
+main {
+    background-color: hsl(0, 0%, 98%);
+}
+</style>
---
+++ rest-countries/src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { computed, ref } from 'vue'
+
+const title = ref('Where In the world?')
+
+const colourScheme = ref(false)
+
+const colourMode = computed(() => {
+  return colourScheme.value ? 'Dark Mode' : 'Light Mode'
+})
+
+function toggleColour() {
+  colourScheme.value = !colourScheme.value
+}
+
+</script>
+
+<template>
+  <nav>
+    <p>{{ title }}</p>
+    <p @click="toggleColour">{{ colourMode }}</p>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+}
+
+p {
+  padding: 1em 5em;
+  font-weight: 800;
+  font-style: normal;
+}
+</style>
