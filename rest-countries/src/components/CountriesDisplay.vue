c2c2c27598d0ca062bf1a7a1696c915a143b3892
MelvinMelonGit <83698963+MelvinMelonGit@users.noreply.github.com>
2024-05-21 00:31:15 +0800

    Added basic UI

--- rest-countries/src/components/CountriesDisplay.vue
+++ rest-countries/src/components/CountriesDisplay.vue
@@ -1,13 +1,24 @@
 <script setup>
 import { ref } from 'vue'
+import Country from './Country.vue'
+
 </script>
 
 <template>
-  <main>this is main</main>
+  <main>
+    <Country />
+  </main>
 </template>
 
 <style scoped>
 main {
-    background-color: hsl(0, 0%, 98%);
+  background-color: hsl(0, 0%, 98%);
+  min-height: 100vh;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+  padding: 5em;
 }
 </style>
---
+++ rest-countries/src/components/Country.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const countries = ref([])
+
+async function getCountries() {
+  const response = await fetch('https://restcountries.com/v3.1/all?fields=name,flags')
+  const data = await response.json()
+  countries.value = data
+}
+
+getCountries()
+
+</script>
+
+<template>
+  <div v-for="country in countries" :key="country.name.official">
+    <p>{{ country.name.official }}</p>
+    <img :src="country.flags.png">
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 100%;
+}
+</style>
